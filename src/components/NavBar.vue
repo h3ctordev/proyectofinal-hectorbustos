adfaedd1f64ef0890c48e8f3d5160b9b889d80d6
h3ct04 <hector@pegasconsentido.cl>
2022-05-25 22:53:48 -0400

    Se restructura el proyecto

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,58 +1,12 @@
 <template>
-  <b-navbar
-    type="dark"
-    variant="dark"
-    class="d-flex justify-content-between px-4"
-    fixed="top"
-  >
-    <b-navbar-brand class="text-white" href="/">AppPedidos</b-navbar-brand>
-
-    <b-button class="ml-auto" variant="outline-success" @click="open = !open">
-      <b-icon icon="cart4"></b-icon> ({{ countItems }})
-    </b-button>
-    <b-modal id="cart-modal" v-model="open" size="lg" scrollable centered>
-      <template #modal-header>
-        <b-container>
-          <b-row align-h="between">
-            <b-col cols="6">
-              <h4>Mi Carro</h4>
-            </b-col>
-            <b-col cols="2" sm="1">
-              <b-button
-                class="text-decoration-none text-black"
-                variant="link"
-                @click="open = false"
-              >
-                <b-icon icon="x"></b-icon>
-              </b-button>
-            </b-col>
-          </b-row>
-        </b-container>
-      </template>
-      <b-table :items="cart" :fields="fields">
-        <template #cell(price)="{ item }"> $ {{ item.price | clp }} </template>
-        <template #cell(total)="{ item }"> $ {{ item.total | clp }} </template>
-        <template #custom-foot>
-          <b-tr>
-            <b-th></b-th>
-            <b-th></b-th>
-            <b-th class="text-end">Total</b-th>
-            <b-th class="text-center"> $ {{ getTotal | clp }}</b-th>
-          </b-tr>
-        </template>
-      </b-table>
-      <template #modal-footer>
-        <div class="text-left">
-          <b-button
-            variant="outline-success"
-            :disabled="countItems === 0"
-            @click="onConfirmBuyout"
-          >
-            Confirmar compra
-          </b-button>
-        </div>
-      </template>
-    </b-modal>
+  <b-navbar type="dark" variant="dark" fixed="top">
+    <b-navbar-brand class="text-white" to="/"> AppPedidos </b-navbar-brand>
+    <b-navbar-nav class="ml-auto">
+      <b-button class="mx-2" variant="success" to="/login"> Login </b-button>
+      <b-button class="ml-auto" variant="outline-success" @click="openCart">
+        <b-icon icon="cart4"></b-icon> ({{ countItems }})
+      </b-button>
+    </b-navbar-nav>
   </b-navbar>
 </template>
 
@@ -66,27 +20,11 @@ export default {
     },
   },
   data() {
-    return {
-      open: false,
-      fields: [
-        { key: "name", label: "Nombre", class: "text-center" },
-        {
-          key: "price",
-          label: "Precio",
-          class: "text-center",
-        },
-        { key: "qty", label: "Cantidad", class: "text-center" },
-        { key: "total", label: "Total", class: "text-center" },
-      ],
-    };
+    return {};
   },
   methods: {
-    onConfirmBuyout() {
-      delete this.cart.__ob__;
-      console.table(this.cart);
-      const total = this.$options.filters.clp(this.getTotal);
-      this.open = false;
-      this.$emit("complete", total);
+    openCart() {
+      this.$emit("open-cart");
     },
   },
   computed: {
